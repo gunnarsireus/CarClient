598eb42a8e1aac234d143b8983c53e4957f7a2f8
KONGUNS <KONGUNS@CLN10907>
2018-01-05 08:30:32 +0100

    Better handling of AspNet.db location

---
+++ CarClient/Services/AspNetDbLocation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarClient.Models;
+
+namespace CarClient.Services
+{
+    // This class is used by the application to get the location of the AspNet.db used for authntication of users.
+    public class AspNetDbLocation : IAspNetDbLocation
+    {
+	    public async Task<string> GetAspNetDbDirectoryAsync()
+	    {
+			return await Utils.Get<string>("api/CurrentDirectory");
+		}
+
+	}
+}
--- CarClient/Startup.cs
+++ CarClient/Startup.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.IO;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -12,60 +13,64 @@ using CarClient.Services;
 
 namespace CarClient
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite("Data Source="+ Configuration["ConnectionStrings:SqLiteConnection"] + "//AspNet.db"));
-
-			services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
-
-            // Add application services.
-            services.AddTransient<IEmailSender, EmailSender>();
-
-            services.AddMvc();
-
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseBrowserLink();
-                app.UseDatabaseErrorPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-
-            app.UseStaticFiles();
-
-            app.UseAuthentication();
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    "default",
-                    "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
-    }
-	public class AppSettings
+	public class Startup
 	{
-		public string SqLiteConnection { get; set; }
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddIdentity<ApplicationUser, IdentityRole>()
+				.AddEntityFrameworkStores<ApplicationDbContext>()
+				.AddDefaultTokenProviders();
+
+			// Add application services.
+			services.AddTransient<IEmailSender, EmailSender>();
+			services.AddMvc();
+
+			Task task = ConfigureServicesAsync(services);
+			task.Wait();
+		}
+		public async Task ConfigureServicesAsync(IServiceCollection services)
+		{
+			var aspNetDbLocation = new AspNetDbLocation();
+			var aspNetDbDirectory = await aspNetDbLocation.GetAspNetDbDirectoryAsync() + "\\App_Data\\AspNet.db";
+
+			services.AddDbContext<ApplicationDbContext>(options =>
+				options.UseSqlite("Data Source=" + aspNetDbDirectory));
+
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+				app.UseBrowserLink();
+				app.UseDatabaseErrorPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+			}
+
+			app.UseStaticFiles();
+
+			app.UseAuthentication();
+
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					"default",
+					"{controller=Home}/{action=Index}/{id?}");
+			});
+		}
 	}
 }
